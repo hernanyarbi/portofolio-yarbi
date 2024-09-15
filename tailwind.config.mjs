2bc7f0e258b19c9ed22e77d7192b83fb9dbc5a9c
Hernan Yarbi <hernan.yarbi@gmail.com>
2024-09-14 22:01:00 -0300

    added Header, ExperienceSection and HeroSection components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: false,
 	theme: {
 		extend: {},
+		borderRadius: {
+			'blob': '48% 52% 67% 33% / 33% 39% 61% 67%',
+			'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '0.25rem',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+		}
 	},
 	plugins: [],
 }
